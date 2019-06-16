94c9b9cd3b0efa4576aea94649ce539b6b8e9c03
Elshad <safarov.elshad007@gmail.com>
2019-06-16 21:10:27 +0500

    Added keyDownHandler and keyUpHandler method in index.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,8 @@
-const InitialState  = {
+const InitialState = Object.freeze({
     BALL_RADIUS: 10,
     FRAME_TIME: 10,
-}
+    PADDLE_HEIGHT: 10,
+    PADDLE_WIDTH: 75,
+});
 
 export default InitialState;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import InitialState  from './constants';
+import InitialState from './constants';
 
 import 'reset-css';
 import './css/index.css';
@@ -8,13 +8,41 @@ const ctx = canvas.getContext('2d');
 
 let x = canvas.width / 2;
 let y = canvas.height - 30;
-
 let dx = 2;
 let dy = -2;
+let rightPressed = false;
+let leftPressed = false;
+let paddleX = (canvas.width - InitialState.PADDLE_WIDTH) / 2;
+
+const keyDownHandler = function(evt) {
+    if (evt.keyCode === 39) {
+        rightPressed = true;
+    }
+    else if (evt.keyCode === 37) {
+        leftPressed = true;
+    }
+};
+
+const keyUpHandler = function(evt){
+    if (evt.keyCode === 39) {
+        rightPressed = false;
+    }
+    else if (evt.keyCode === 37) {
+        leftPressed = false;
+    }
+};
 
 const drawBall = () => {
     ctx.beginPath();
-    ctx.arc(x, y, InitialState .BALL_RADIUS, 0, Math.PI*2);
+    ctx.arc(x, y, InitialState.BALL_RADIUS, 0, Math.PI * 2);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
+};
+
+const drawPaddle = () => {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - InitialState.PADDLE_HEIGHT, InitialState.PADDLE_WIDTH, InitialState.PADDLE_HEIGHT);
     ctx.fillStyle = '#0095DD';
     ctx.fill();
     ctx.closePath();
@@ -23,15 +51,26 @@ const drawBall = () => {
 const draw = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-    x += dx;
-    y += dy;
-    if(y + dy > canvas.height - InitialState .BALL_RADIUS || y + dy < InitialState .BALL_RADIUS){
+    drawPaddle();
+
+    if (y + dy > canvas.height - InitialState.BALL_RADIUS || y + dy < InitialState.BALL_RADIUS) {
         dy = -dy;
     }
-
-    if(x + dx > canvas.width - InitialState .BALL_RADIUS || x + dx < InitialState .BALL_RADIUS){
+    if (x + dx > canvas.width - InitialState.BALL_RADIUS || x + dx < InitialState.BALL_RADIUS) {
         dx = -dx;
     }
+
+    if (rightPressed && paddleX < canvas.width - InitialState.PADDLE_WIDTH) {
+        paddleX += 7;
+    }
+    else if (leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+
+    x += dx;
+    y += dy;
 };
 
-setInterval(draw, InitialState .FRAME_TIME);
+setInterval(draw, InitialState.FRAME_TIME);
+document.addEventListener('keydown', keyDownHandler);
+document.addEventListener('keyup', keyUpHandler);
